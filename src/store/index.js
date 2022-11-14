1140bc4f1b71abd1f39beb3060269b2855a9a22e
Redditask <artyomageyko@gmail.com>
2022-11-15 00:56:56 +0300

    added saving tasks to localStorage with react-persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,37 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import taskReducer from "./taskManagerSlice";
+import storage from "redux-persist/lib/storage";
+import {
+    persistReducer,
+    persistStore,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
 
-export default configureStore({
-    reducer: {
-        tasks: taskReducer
-    }
+const rootReducer = combineReducers({
+    tasks: taskReducer,
 });
+
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
+
+export const persistor = persistStore(store);
+export default store;
